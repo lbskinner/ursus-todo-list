cf364067285f77453c954199468e1e2b84f566a2
Leila Skinner <beileiwang@gmail.com>
2020-03-18 15:01:56 -0500

    toggles the boolean

--- client.js
+++ client.js
@@ -3,9 +3,11 @@ $(document).ready(init);
 const tasks = [];
 
 function init() {
-  console.log("ready");
+  console.log("READY");
   // add event listener for add task button
   $(".js-add-task").on("click", addTask);
+  // add event listener for complete button
+  $(".js-todo-list").on("click", ".js-btn-complete", clickCompleteButton);
 }
 
 function addTask() {
@@ -28,9 +30,25 @@ function render() {
   console.log("RENDER");
   // empty ul
   $(".js-todo-list").empty();
-  for (let task of tasks) {
+  for (let i = 0; i < tasks.length; i++) {
     $(".js-todo-list").append(`
-      <li>${task.name}<button class="js-btn-complete">Complete</button></li>
+      <li>${tasks[i].name}<button class="js-btn-complete" data-index="${i}">Complete</button></li>
       `);
   }
+}
+
+function clickCompleteButton() {
+  console.log("COMPLETE BUTTON CLICKED");
+  // check what is "this"
+  console.log(this);
+  // find the index of "this" task
+  const taskIndex = $(this).data("index");
+  console.log(taskIndex);
+  // checks the boolean value of the task
+  console.log(tasks[taskIndex].completed);
+  // toggles the boolean value
+  tasks[taskIndex].completed = !tasks[taskIndex].completed;
+  // check the boolean value of the task again
+  console.log(tasks[taskIndex].completed);
+  render();
 }
